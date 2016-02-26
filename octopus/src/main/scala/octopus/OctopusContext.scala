0ad851d4e66d8055040bdcfa81c1116b5d920b45
Ulysse Mizrahi <umizrahi@nextperf.com>
2016-02-26 11:27:10 +0100

    refactors octopus context generation to tie an octopus context to a given spark context. Makes constructor private to prevent multiple initialization.

--- octopus/src/main/scala/octopus/OctopusContext.scala
+++ octopus/src/main/scala/octopus/OctopusContext.scala
@@ -1,12 +1,14 @@
 package scala.octopus
 
+import java.util.concurrent.atomic.AtomicReference
+
 import main.scala.octopus.{TextDataSet, DataSet, DeployedDataSet}
 import org.apache.spark.SparkContext
 
 /**
  * Created by umizrahi on 25/02/2016.
  */
-class OctopusContext(sc: SparkContext) {
+class OctopusContext private(sc: SparkContext) {
   def deploy[T](data: Iterable[T]): DataSet[T] = new DeployedDataSet(data)(sc)
 
   def textFile(file: java.io.File): DataSet[String] = new TextDataSet(file)(sc)
@@ -21,8 +23,39 @@ class OctopusContext(sc: SparkContext) {
 
 object OctopusContext {
 
+  private class ContextMapping() {
+    var sparkContext: SparkContext = null
+    var octopusContext: OctopusContext = null
+
+    def set(sc: SparkContext, oc: OctopusContext) = this.synchronized {
+      require(sc != null)
+      require(oc != null)
+      if (sparkContext != null) throw new IllegalStateException("Only one octopus context may be active at a time")
+      sparkContext = sc
+      octopusContext = oc
+    }
+
+    def getOctopusContext = this.synchronized {
+      octopusContext
+    }
+
+    def getSparkContext = this.synchronized {
+      sparkContext
+    }
+
+  }
+
+  private[octopus] val contextMapping = new ContextMapping()
+
   implicit class Make(sc: SparkContext) {
-    def getOctopusContext = new OctopusContext(sc)
+    def getOctopusContext = contextMapping.synchronized {
+      var oc = contextMapping.getOctopusContext
+      if (oc == null) {
+        oc = new OctopusContext(sc)
+        contextMapping.set(sc, oc)
+      }
+      oc
+    }
   }
 
 }
