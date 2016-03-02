b2396487bfd7ae1964e16f2d6e07db51fd8b7f59
Ulysse Mizrahi <umizrahi@nextperf.com>
2016-03-02 17:44:19 +0100

    adds better support for asynchronous job launch (may not utilize cluster fully, should be tested on a cluster ; but works locally)

--- octopus/src/main/scala/octopus/OctopusContext.scala
+++ octopus/src/main/scala/octopus/OctopusContext.scala
@@ -1,11 +1,11 @@
 package scala.octopus
 
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent
 import java.util.concurrent.{Callable, Executors}
 
 import org.apache.spark.SparkContext
 
-import scala.collection.mutable
+import scala.concurrent.Future
 
 
 /**
@@ -16,15 +16,15 @@ import scala.collection.mutable
  */
 class OctopusContext private(sc: SparkContext) {
 
-  import scala.octopus.OctopusContext._
-
   def getSparkContext = sc
 
   def deploy[T](data: Iterable[T]): DataSet[T] = new DeployedDataSet(data, this)
 
   def textFile(file: java.io.File): DataSet[String] = new TextDataSet(file)(this)
 
-  private val jobExecutor = Executors.newSingleThreadExecutor()
+  private val jobExecutor = Executors.newCachedThreadPool()
+
+  // private val jobExecutor = Executors.newSingleThreadExecutor()
 
   def runJobs[T](jobs: Seq[() => T]) = {
     val dummy = deploy(Iterable(1))
@@ -42,21 +42,28 @@ class OctopusContext private(sc: SparkContext) {
     submitJobsOnDataSet(dummy, mappedJobs)
   }
 
-  def submitJobsOnDataSet[T, S](data: DataSet[T], jobs: Seq[Iterable[T] => S]) = {
-    jobExecutor.submit(new Callable[Seq[S]] {
-      override def call(): Seq[S] = {
-        implicit val cachedIds = cachedRegister.getRegistrationStatuses
-        val nJobs = jobs.length
-        val bcData = getSparkContext.broadcast(data)
-        val jobTasks = jobs.map(j => new DeployedTask[Iterable[T] => S](j))
-        getSparkContext.parallelize(1 to nJobs, nJobs)
-          .mapPartitionsWithIndex { case (i, dat) =>
-          Iterator((i, jobTasks(i).getTask(bcData.value.getData)))
+  def submitJobsOnDataSet[T, S](data: DataSet[T], jobs: Seq[Iterable[T] => S]): concurrent.Future[Seq[S]] = {
+
+    if (jobs.isEmpty) {
+      jobExecutor.submit(new Callable[Seq[S]] {
+        override def call(): Seq[S] = Seq()
+      })
+    } else {
+      jobExecutor.submit(new Callable[Seq[S]] {
+        override def call(): Seq[S] = {
+          implicit val cachedIds = cachedRegister.getRegistrationStatuses
+          val nJobs = jobs.length
+          val bcData = getSparkContext.broadcast(data)
+          val jobTasks = jobs.map(j => new DeployedTask[Iterable[T] => S](j))
+          getSparkContext.parallelize(1 to nJobs, nJobs)
+            .mapPartitionsWithIndex { case (i, dat) =>
+            Iterator((i, jobTasks(i).getTask(bcData.value.getData)))
+          }
+            .collect()
+            .sortBy(_._1).map(_._2).toSeq
         }
-          .collect()
-          .sortBy(_._1).map(_._2).toSeq
-      }
-    })
+      })
+    }
   }
 
   private val dataSetRegister = new Register
@@ -80,7 +87,6 @@ class OctopusContext private(sc: SparkContext) {
 }
 
 object OctopusContext {
-
 
 
   private class ContextMapping() {
